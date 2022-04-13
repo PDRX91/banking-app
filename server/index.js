b8c15dd47e7a51163267a226b9665411aa657457
PDRX91 <parker.rebensdorf@gmail.com>
2022-04-13 10:20:26 -0700

    db setup WIP

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://PDRRoot:bDjoyaBVebSGJ628@cluster0.hj0fu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: "Back to the Future" };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 const PORT = process.env.PORT || 3001;
 
